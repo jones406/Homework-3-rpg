72565e8c08165021e8017eab144a761f295afe10
brooke <brookejones406@gmail.com>
2021-09-25 22:02:20 -0500

    final-readytosubmit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment Code
-
 //select button element from html file (button id="generate")
 var generateBtn = document.querySelector("#generate");
 
@@ -12,41 +11,51 @@ var specialChar = ["!", "#", "$", "%", "&", "'", "\"", "(", ",", ")", "*", "+", 
 //create a function to generate a random pwd
 function generatePassword() {
   let yourPassword = "";
-  let pwdLength = prompt("Type a number between 8 and 128.");
- 
+  let pwdLength = "";
+
   //check for user error before moving forward
   function pwdCheck() {
-    if (pwdLength > 7 && pwdLength < 129 && pwdLength != isNaN) {
+    pwdLength = prompt("Type a number between 8 and 128.");
+    if (pwdLength < 8 || pwdLength > 128 || pwdLength == null || pwdLength == false) {     
+      alert("Hmm let's try that again!");
+      pwdCheck();
+    } else {
+    return;
+    } 
+  }
+  pwdCheck();
+  alert("Great. Next, select what character types to include. Select OK for Yes and Cancel for no. Be sure to say OK to least one!")
+  //log user's pwd length:
+  console.log("Chosen length: ", pwdLength);
+
+ //user chooses character types
+//check for user error before moving forward (confirms return true or false)
+  let ucSelect = "";
+  let lcSelect = "";
+  let numSelect = "";
+  let specialSelect = "";
+   
+  function pwdCheck2() {
+    ucSelect = confirm("Would you like to include uppercase letters?");  
+    lcSelect = confirm("Would you like to include lowercase letters?");
+    numSelect = confirm("Would you like to include numbers?");
+    specialSelect = confirm("Would you like to include special characters?");
+    if (ucSelect == false && lcSelect == false && numSelect == false && specialSelect == false) {
+      alert("Remember, you need to choose at least one!");
+      pwdCheck2();
+    } else {
       return;
     }
-    pwdCheck()
-  } 
-  pwdCheck();
-
-  //log user's pwd length:
-  //console.log("Chosen length: ", pwdLength);
-
-  let ucSelect = confirm("Would you like to include uppercase letters?");  
-  let lcSelect = confirm("Would you like to include lowercase letters?");
-  let numSelect = confirm("Would you like to include numbers?");
-  let specialSelect = confirm("Would you like to include special characters?");
-  
-  //check for user error before moving forward
-  function pwdCheck2() {
-    if (ucSelect == false && lcSelect == false && numSelect == false && specialSelect == false) {
-      alert("pick at least one - now start over");
-      }
-      pwdCheck();
   }
   pwdCheck2();    
 
   //log user's answers:
-  //console.log("Include upper: ", ucSelect);
-  //console.log("Include lower: ", lcSelect);
-  //console.log("Include numeric: ", numSelect);
-  //console.log("Include special: ", specialSelect);
+  console.log("Include upper: ", ucSelect);
+  console.log("Include lower: ", lcSelect);
+  console.log("Include numeric: ", numSelect);
+  console.log("Include special: ", specialSelect);
 
-  //Check which arrays we access based on choices user made. Chosen arrays will be added into new array
+  //Check which arrays we access based on user choices, then add those into a new array.
   let charSelection = []
   if (ucSelect) { //don't need to include ""=== true" after upperSelect, it's implied
     charSelection = charSelection.concat(ucLetters); //or why wouldn't we
@@ -59,17 +68,16 @@ function generatePassword() {
   } 
   if (specialSelect) {
     charSelection = charSelection.concat(specialChar);
-  } 
+  }
 
-  console.log("characters:", charSelection); // log our new array in the console
-
-  //loop thru my charSelection array and pick randomly however many times was specified by pwdLength
+//loop thru my charSelection array and pick randomly however many times was specified by pwdLength
   for (let i = 0; i < pwdLength; i++) {
     yourPassword += charSelection[Math.floor(Math.random()*charSelection.length)];
   }
   return yourPassword
 }
-    // Write password to the #password input
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
